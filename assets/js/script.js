1e232c44b518b8d597f5131e20e7ae5f266e6863
Faisal Mohammed <40631753+faisal244@users.noreply.github.com>
2022-04-27 02:59:01 +0100

    More pseudo code, working on game logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,42 +7,80 @@ const score = {
    ties:0,
 };
 
+const choices = ["R", "P", "S"];
 
+// get the users choice by using a prompt
+   const getUserChoice = function () {
+      // prompt the user to enter Choice
+      const userChoice = prompt ("Please select Rock (R) , Paper (P) or Scissors (S)");
+      
+      // validate the Choice
+      // if not valid, then present an alert
 
-const playGame = function () {
-
-   const getUserChoice = function (){
-      return "R";
+      if (userChoice === "R" || userChoice === "P" || userChoice === "S");{
+      return userChoice;
+    } else {
+       alert("Please select one of the following - R, P or S");
    }
+};
    
+   // get the random computer choice
    const getComputerChoice = function (){
       return "P";
+      const randomindex = math.floor(Math.random() * (choices.length) + 0)
+      return choices[randomindex];
    }
-   
+   //  for the 2 player choice check game logic
    const gameLogic = function (player1, player2) {
+      if (player1 === "R" && player2 === "R") {
+         return 'tie'
+      }
       return "win";
    }
    
+   // update player 1 score depending on outcome
    const updateScore = function (gameOutcome) {
       // update wins if win
       // update losses if loses
       // update ties if tie
    }
    
+   // display the score of player 1 in the console logs
    const displayScore = function () {
       // console log the score 
    
    }
+const playGame = function () {
+//  get player 1 choice 
+const player1Choice = getUserChoice();
+console.log("player 1 choice", player1Choice)
 
+// get the computer choice
+const computerChoice = getComputerChoice();
+console.log("Computer Choice ", computerChoice)
+
+// feed the player choice and computure choice to game logic 
+const gameOutcome = gameLogic(player1Choice, computerChoice);
+console.log("Game Outcome", gameOutcome);
+
+// update the scores
+updateScore(gameOutcome);
+
+// display the scores
+displayScore();
+console.log("score: W - " + score.wins + " | L - " + score.losses + " | T - " + score.ties)
 }
 
+// present the user with a confirm modal - yes/no
 const playAgain = function() {
+   // user will be presented with a confirm prompt
    return false;
 }
 
 // declare a variable to track game in progress
 let isGameInProgress = true;
 
+// start the game by calling this function
 while (isGameinprogress) {
    playGame()
 }
