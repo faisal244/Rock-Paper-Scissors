77020dbee86562cbd65ffd156a81574dea99531f
Faisal Mohammed <40631753+faisal244@users.noreply.github.com>
2022-04-27 18:17:57 +0100

    Fixed issues with updating and displaying score
    - Cleaned up code
    - Completed defining game logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,135 +1,157 @@
-// declare the game object
-const game = {
-   inProgress: false,
-   score: {
-     wins: 0,
-     losses: 0,
-     ties: 0,
-   },
-   validChoices: ["R", "P", "S"],
-   displayScore: function () {
-     console.log(this.score);
-   },
-   startGame: function () {
-     // present confirm
-     const confirmChoice = confirm("Are you ready to play?");
- 
-     // if yes set inProgress to true
-     if (confirmChoice) {
-       this.inProgress = true;
-     }
- 
-     // return confirm boolean
-     return confirmChoice;
-   },
-   getUserChoice: function () {
-     // prompt user for R, P or S
-     const promptInput = prompt(
-       "Please select one of the following: rock (R) | paper (P) | scissors (S)"
-     );
- 
-     // validate if input is included in validChoices array
-     const isValid = this.validChoices.includes(promptInput.toUpperCase());
-     if (isValid) {
-       return promptInput.toUpperCase();
-     } else {
-       // show alert
-       alert("Please enter a valid value");
-     }
-   },
-   getComputerChoice: function () {
-     // get random index between 0 and 2
-     const randomIndex = Math.floor(
-       Math.random() * this.validChoices.length + 0
-     );
- 
-     // return random choice
-     return this.validChoices[randomIndex];
-   },
-   continueGame: function () {
-     // present confirm
-     const confirmChoice = confirm("Do you want to play again?");
- 
-     // if yes set inProgress to true
-     if (!confirmChoice) {
-       this.inProgress = false;
-     }
-   },
-   getGameOutcome: (userChoice, computerChoice) => {
-     // return string "wins" or "losses" or "ties"
-     if (userChoice === "R" && computerChoice === "R") {
-       return "ties";
-     }
- 
-     if (userChoice === "R" && computerChoice === "P") {
-       return "losses";
-     }
- 
-     if (userChoice === "R" && computerChoice === "S") {
-       return "wins";
-     }
- 
-     if (userChoice === "P" && computerChoice === "R") {
-       return "wins";
-     }
- 
-     if (userChoice === "P" && computerChoice === "P") {
-       return "ties";
-     }
- 
-     if (userChoice === "P" && computerChoice === "S") {
-       return "losses";
-     }
- 
-     if (userChoice === "S" && computerChoice === "R") {
-       return "losses";
-     }
- 
-     if (userChoice === "S" && computerChoice === "P") {
-       return "wins";
-     }
- 
-     if (userChoice === "S" && computerChoice === "S") {
-       return "ties";
-     }
-   },
-   updateScore: function (gameOutcome) {
-     this.score[gameOutcome] += 1;
-   },
- };
- 
- // MAIN APP
- const confirmStartGame = game.startGame();
- 
- if (confirmStartGame) {
-   // TODO: start loop here
- 
-   while (game.inProgress) {
-     // get the user's choice
-     const userChoice = game.getUserChoice();
- 
-     // if userChoice is valid
-     if (userChoice) {
-       // get computer choice
-       const computerChoice = game.getComputerChoice();
- 
-       // get game outcome
-       const gameOutcome = game.getGameOutcome(userChoice, computerChoice);
- 
-       // update score after game outcome
-       game.updateScore(gameOutcome);
- 
-       // display score after update
-       game.displayScore();
- 
-       // ask user if continue
-       game.continueGame();
-     }
+
+const score = {
+   wins: 0,
+   losses: 0,
+   ties: 0,
+};
+
+const choices = [ 'R', 'P', 'S'];
+
+// get the users choice by using a prompt
+   const getUserChoice = function () {
+      // prompt the user to enter Choice
+      const userChoice = prompt ("Please select Rock (R) , Paper (P) or Scissors (S)").toUpperCase();;
+      
+      if (!userChoice) {
+         isGameInProgress = false;
+      } else {
+         // validate the Choice
+         // if not valid, then present an alert
+         
+          // validate if input is included in validChoices array
+         if (userChoice === 'R' || userChoice === 'P' || userChoice === 'S') {
+               
+    
+         return userChoice;
+      } else {
+          alert("Please select one of the following - R, P or S");
+      }
    }
- 
-   console.log("YOUR FINAL SCORE");
-   console.table(game.score);
- } else {
-   // display message
-   console.log("Maybe try again!!");
- }
+};
+   
+   
+   // get the random computer choice
+   const getComputerChoice = function () {
+      // generate an index between 0 and 2 
+      const randomindex = Math.floor (Math.random() * choices.length + 0);
+      return choices[randomindex];
+   };
+
+
+
+   //  for the 2 player choice check game logic
+   const gameLogic = function (player1, player2) {
+      if (player1 === "R" && player2 === "P") {
+         return 'lose';
+      }
+      if (player1 === "R" && player2 === "S") {
+         return 'win';
+      }
+      
+      if (player1 === "P" && player2 === "R") {
+         return 'win';
+      }
+
+      if (player1 === "P" && player2 === "S") {
+         return 'lose';
+      }
+
+      if (player1 === "S" && player2 === "R") {
+         return 'lose';
+      }
+
+      if (player1 === "S" && player2 === "P") {
+         return 'win';
+      }
+
+      return 'tie';
+   };
+   
+   // update player 1 score depending on outcome
+   const updateScore = function (gameOutcome) {
+      // update wins if win
+      // update losses if loses
+      // update ties if tie
+      if (gameOutcome === 'win') {
+         score.wins +=1;
+      }
+
+      if (gameOutcome === 'lose') {
+         score.losses +=1;
+
+      } if (gameOutcome === 'tie') {
+            score.ties +1;
+   };
+};
+   
+   
+   // display the score of player 1 in the console logs
+   const displayScore = function (gameOutcome) {
+      console.log('Game outcome for player 1: ' + gameOutcome);
+      // console log the score 
+      console.log(
+         'Score: W -' + 
+         score.wins + 
+         ' | L - ' + 
+         score.losses +
+         ' | T - ' + 
+         score.ties
+      );
+   
+   };
+
+
+
+const playGame = function () {
+//  get player 1 choice 
+const player1Choice = getUserChoice();
+console.log("player 1 plays: " + player1Choice)
+
+// get the computer choice
+const computerChoice = getComputerChoice();
+console.log("Computer plays: " + computerChoice)
+
+if (player1Choice && computerChoice) {
+   // feed the player choice and computure choice to game logic 
+   const gameOutcome = gameLogic(player1Choice, computerChoice);
+   // console.log("Game Outcome", gameOutcome);
+   // update the scores
+   updateScore(gameOutcome);
+   
+   // display the scores
+   displayScore(gameOutcome);
+   // console.log("score: W - " + score.wins + " | L - " + score.losses + " | T - " + score.ties)
+}
+
+
+};
+
+// present the user with a confirm modal - yes/no
+const playAgain = function() {
+   const isPlayAgain = confirm('do you want to play again?')
+   // user will be presented with a confirm prompt
+   return isPlayAgain;
+}
+
+// declare a variable to track game in progress
+let isGameInProgress = true;
+
+// start the game by calling this function
+while (isGameInProgress) {
+   playGame();
+}
+
+if (isGameInProgress) {
+   // ask user if they want to continue
+   const isPlayAgain = playAgain()
+   
+   // if playAgain is false, set isGameinprogress to false
+   if (!isPlayAgain) {
+      isGameInProgress = false;
+   }
+
+
+   
+}
+
+playGame ();
